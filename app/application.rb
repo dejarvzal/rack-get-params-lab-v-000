7d362534b956907c0fc2bf3e3f7a6787e6f111bd
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 06:59:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
       end
     elsif
         resp.write "Your cart is empty"
-      end
+      # end
       elsif req.path.match(/add/)
           new_item = req.params["item"]
         if @@items.include? new_item

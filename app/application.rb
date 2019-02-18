939d9e9e71fe2359aa7baebc2974d1247486e270
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 07:03:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,35 +9,34 @@ class Application
 
     if req.path.match(/items/)
       @@items.each do |item|
+      resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
         resp.write "#{item}\n"
         end
-      elsif req.path.match(/search/)
-        search_term = req.params["q"]
-        resp.write handle_search(search_term)
-      elsif req.path.match(/cart/)
-        if @@cart.empty?
-          resp.write "Your cart is empty"
-        else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
       end
-    elsif
-        resp.write "Your cart is empty"
-      # end
-      elsif req.path.match(/add/)
-          new_item = req.params["item"]
-        if @@items.include? new_item
-          @@cart << new_item
-          resp.write "added #{item_to_add}"
-        else
-          resp.write "We don't have that item!"
-        end
-        else
-          resp.write "Path Not Found"
-        end
-
-    resp.finish
+    # elsif
+    #   resp.write "Your cart is empty"
+    #     # end
+        elsif req.path.match(/add/)
+            new_item = req.params["item"]
+          if @@items.include? new_item
+            @@cart << new_item
+            resp.write "added #{item_to_add}"
+          else
+            resp.write "We don't have that item!"
+          end
+          else
+            resp.write "Path Not Found"
+          end
+      resp.finish
   end
 
   def handle_search(search_term)
